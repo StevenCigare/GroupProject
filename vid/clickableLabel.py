1d25ddc59d3977fed48dd67c00e9e5bd16bcbf59
Michal Turek <87382140+MichalTurek@users.noreply.github.com>
2023-01-15 18:15:30 +0100

    Revert "added possibility to choose frame"

--- vid/clickableLabel.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtCore import Signal
-from PySide6.QtWidgets import QLabel
-
-
-class ClickableLabel(QLabel):
-    clicked = Signal()
-
-    def mousePressEvent(self, ev):
-        self.clicked.emit()
